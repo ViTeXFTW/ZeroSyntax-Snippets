dda77a8d1f3ffe2d625b4b1d3d2c2c12ca717ef9
ViTeXFTW <mads213@gmail.com>
2024-01-18 16:28:25 +0100

    Delete extension.ts file

--- src/extension.ts
+++
@@ -1,5 +0,0 @@
-import * as vscode from 'vscode';
-
-export function activate(context: vscode.ExtensionContext) {
-    console.log('Congratulations, your extension "ZeroSyntax-Snippets" is now active!');
-}
